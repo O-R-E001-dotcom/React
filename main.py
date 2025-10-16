21108b6e5db48b79378af35d88d6cbc5e246a588
Ore <aboloresanni001@gmail.com>
2025-10-16 20:37:09 +0100

    newly

--- main.py
+++
@@ -1,5 +0,0 @@
-import mymodule
-
-print(mymodule.name("Ore"))
-print(mymodule.add(1, 6))
-print(mymodule.subtract(8, 6))
--- mymodule.py
+++
@@ -1,8 +0,0 @@
-def greet(name):
-    return f"Hello, my name is {name}!"
-
-def add(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
